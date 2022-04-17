0dbd54d95498fe5c547e50044e301a504ed97302
CenXi <1850044540@qq.com>
2022-04-17 23:14:25 +0800

    first-commit

---
+++ src/main/java/com/zhiwuheng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhiwuheng.git;
+
+/**
+ * @author: CenXi
+ * @Date: 2022-04-17.04.17.23:10
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
